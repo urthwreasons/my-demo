23904087b4521e0e65773d3493ea6710b1df1de3
witthawit <witthawit0123ppok@gmail.com>
2025-07-09 23:42:40 +0700

    Add manifest and icons for PWA

---
+++ service-worker.js
@@ -0,0 +1,6 @@
+self.addEventListener('install', event => {
+  console.log('Service Worker installed');
+});
+self.addEventListener('activate', event => {
+  console.log('Service Worker activated');
+});
